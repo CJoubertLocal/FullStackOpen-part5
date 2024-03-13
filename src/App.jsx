25dec80e54f742219fcc4573b2df21ff6244b540
CJoubertLocal <chrinnetwrok@gmail.com>
2024-03-13 17:31:37 +0000

    Exercise 5.1, user login form and service added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [notificationMessage, setNotificationMessage] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +18,59 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({username, password})
+      setUser(user)
+      setUsername('')
+      setPassword('')
+
+    } catch (exception) {
+      console.log('username or password is incorrect')
+      // setNotificationMessage('username or password is incorrect')
+      // setTimeout(() => {
+      //   setNotificationMessage(null)
+      // }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <>
+      <LoginForm 
+          username={username}
+          setUsername={setUsername}
+          password={password}
+          setPassword={setPassword}
+          handleLogin={handleLogin}
+        />
+    </>
+  )
+
+  const blogForm = () => (
+    <>
+      <div>
+        {user.username} logged in
+      </div>
+      <div>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    </>
+  )
+
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+
+      {
+        user === null 
+        ? loginForm() 
+        : blogForm()
+      }
+
     </div>
   )
 }
---
+++ src/components/Notification.jsx
@@ -0,0 +1,36 @@
+
+const Notification = ({message, useSuccessStyle}) => {
+    const sucessStyle = {
+      color: 'green',
+      background: 'lightgreen',
+      fontSize: 20,
+      borderStyle: 'solid',
+      borderRadius: 5,
+      padding: 10,
+      marginBottom: 10
+    }
+  
+    const errorStyle = {
+      color: 'red',
+      background: 'lightred',
+      fontSize: 20,
+      borderStyle: 'solid',
+      borderRadius: 5,
+      padding: 10,
+      marginBottom: 10
+    }
+  
+    const notificationStyle = useSuccessStyle ? sucessStyle : errorStyle
+  
+    if (message === null) {
+        return null
+    }
+  
+    return (
+        <div style={notificationStyle}>
+            {message}
+        </div>
+    )
+  }
+
+  export default Notification
